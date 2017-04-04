64b48e70072597f5f8b9f7a0d6ed7334a30a82f2
Raquel Rahmey <raquelrahmey@Raquels-MacBook-Pro.local>
2017-04-04 12:20:55 -0400

    Added Pod Files

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'MakeUp App' do
+	pod 'SwiftyJSON'
+  
+    pod 'Alamofire', '~> 4.4'
+end
---
+++ Pods/Target Support Files/Pods-MakeUp App/Pods-MakeUp App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MakeUp_App : NSObject
+@end
+@implementation PodsDummy_Pods_MakeUp_App
+@end
