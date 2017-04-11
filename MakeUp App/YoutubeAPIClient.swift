2a0cfdc1445191eda5c9e558f450426623808627
Ben Bernstein <bb@burningflowe.rs>
2017-04-11 11:22:40 -0400

    youtube search truncate string after white space

--- MakeUp App/YoutubeAPIClient.swift
+++ MakeUp App/YoutubeAPIClient.swift
@@ -14,13 +14,24 @@ final class YoutubeAPIClient {
     
     class func searchYoutubeVideos(search: String, type: YoutubeSearch, completion: @escaping ([JSON], String) -> ()) {
         let baseUrl = "https://www.googleapis.com/youtube/v3/search?key=\(Secrets.youTubeKey)&part=snippet&type=video&maxResults=50&q="
-        let tutorialSearch = search + type.rawValue
-        let validSearch = tutorialSearch.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        
+        
+        let spaceCount = search.characters.filter { $0 == " " }.count
+        var checkedSearch = ""
+        if spaceCount > 4 {
+            checkedSearch = truncateStringAfterWhiteSpace(string: search, words: 4)
+        } else {
+            checkedSearch = search
+        }
+        
+        let combinedSearch = checkedSearch + type.rawValue
+        print("combinedSearch is", combinedSearch)
+
+        let validSearch = combinedSearch.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
         
         print("valid search is", validSearch)
         
         let url = baseUrl + validSearch!
-        print("url", url)
         Alamofire.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).validate().responseJSON { (response) in
             if let data = response.data {
                 let json = JSON(data: data)
@@ -32,6 +43,22 @@ final class YoutubeAPIClient {
     
     
     
+}
+
+func truncateStringAfterWhiteSpace(string: String, words: Int) -> String {
+    var whiteCount = 0
+    var whiteSpacePosition = 0
+    for (index, value) in Array(string.characters).enumerated() {
+        if value == " " {
+            whiteCount += 1
+            if whiteCount == words {
+                whiteSpacePosition = index
+            }
+        }
+    }
+    let startIndex = string.startIndex
+    let endIndex = string.index(startIndex, offsetBy: whiteSpacePosition)
+    return string[startIndex...endIndex]
 }
 
 enum YoutubeSearch: String {
