daab28c5c451c97c8e78526f2ac7e987c4a317db
Ben Bernstein <bb@burningflowe.rs>
2017-04-11 11:28:14 -0400

    optimize youtube truncate function

--- MakeUp App/YoutubeAPIClient.swift
+++ MakeUp App/YoutubeAPIClient.swift
@@ -15,18 +15,11 @@ final class YoutubeAPIClient {
     class func searchYoutubeVideos(search: String, type: YoutubeSearch, completion: @escaping ([JSON], String) -> ()) {
         let baseUrl = "https://www.googleapis.com/youtube/v3/search?key=\(Secrets.youTubeKey)&part=snippet&type=video&maxResults=50&q="
         
-        
-        let spaceCount = search.characters.filter { $0 == " " }.count
-        var checkedSearch = ""
-        if spaceCount > 4 {
-            checkedSearch = truncateStringAfterWhiteSpace(string: search, words: 4)
-        } else {
-            checkedSearch = search
-        }
+        let checkedSearch = truncateStringAfterWhiteSpace(string: search, words: 4)
         
         let combinedSearch = checkedSearch + type.rawValue
         print("combinedSearch is", combinedSearch)
-
+        
         let validSearch = combinedSearch.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
         
         print("valid search is", validSearch)
@@ -48,17 +41,24 @@ final class YoutubeAPIClient {
 func truncateStringAfterWhiteSpace(string: String, words: Int) -> String {
     var whiteCount = 0
     var whiteSpacePosition = 0
+    var shouldBeCut = false
     for (index, value) in Array(string.characters).enumerated() {
         if value == " " {
             whiteCount += 1
             if whiteCount == words {
                 whiteSpacePosition = index
+                shouldBeCut = true
             }
         }
     }
     let startIndex = string.startIndex
     let endIndex = string.index(startIndex, offsetBy: whiteSpacePosition)
-    return string[startIndex...endIndex]
+    if shouldBeCut {
+        return string[startIndex...endIndex]
+        
+    } else {
+        return string
+    }
 }
 
 enum YoutubeSearch: String {
