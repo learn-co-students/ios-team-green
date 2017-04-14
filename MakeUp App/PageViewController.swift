00ec84bbf46332aa30b90383ca71bff392148d51
Raquel Rahmey <rrahmey@aol.com>
2017-04-14 10:20:54 -0400

    swipe view added

--- MakeUp App/PageViewController.swift
+++ MakeUp App/PageViewController.swift
@@ -8,28 +8,61 @@
 
 import UIKit
 
-class PageViewController: UIPageViewController {
+class PageViewController: UIPageViewController, UIPageViewControllerDataSource {
+    
+    lazy var viewControllerList: [UIViewController] = {
+        let homeView = HomeViewController()
+        let homeNav = UINavigationController(rootViewController: homeView)
+        let searchView = SearchViewController()
+        let searchNav = UINavigationController(rootViewController: searchView)
+        let resultView = ResultsViewController()
+        let resultNav = UINavigationController(rootViewController: resultView)
+        
+        return [homeNav, searchNav, resultNav]
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.backgroundColor = UIColor.blue
+        
+        dataSource = self
+        
+        if let firstVC = viewControllerList.first{
+        setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+        }
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+        
+        let previousIndex = vcIndex - 1
+        
+        guard previousIndex >= 0 else {return nil}
+        
+        guard viewControllerList.count > previousIndex else {return nil}
+        
+        return viewControllerList[previousIndex]
     }
-    */
 
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+
+        let nextIndex = vcIndex + 1
+        
+        guard viewControllerList.count != nextIndex else {return nil}
+        
+        guard viewControllerList.count > nextIndex else {return nil}
+        
+        return viewControllerList[nextIndex]
+
+    }
+    
+    
 }
+    
+
